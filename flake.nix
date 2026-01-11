eefe2c641e27a98f844c9375c9e51c6b8b21aa0e
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-10 20:53:38 -0500

    Creating multiples machines

--- flake.nix
+++ flake.nix
@@ -32,33 +32,64 @@
         );
 
       forAllSystems = forSystems supportedSystems;
+
+      domain = "cluster.heyk.internal";
+
+      k8sMachines = [
+        { hostName = "k1"; ip = "10.10.2.1"; }
+        { hostName = "k2"; ip = "10.10.2.3"; }
+        { hostName = "k3"; ip = "10.10.2.2"; }
+      ];
+
+      kubernetes = { hostName, ip }:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+
+          modules = [
+            ./hosts/knode
+            disko.nixosModules.disko
+            ./modules/partitions/single-disk-zfs-swap.nix
+            { hardware.facter.reportPath = ./hosts/knode/${hostName}.json; }
+            {
+              networking.hostName = hostName;
+              networking = {
+                interfaces.ens18 = {
+                  ipv4.addresses = [{
+                    address = ip;
+                    prefixLength = 24;
+                  }];
+                };
+                defaultGateway = {
+                  address = "10.10.0.1";
+                  interface = "ens18";
+                };
+              };
+            }
+            comin.nixosModules.comin
+            ({
+              services.comin = {
+                enable = true;
+                remotes = [{
+                  name = "origin";
+                  url = "https://github.com/ph/heyklab.git";
+                }];
+              };
+            })
+          ];
+        };
     in
     {
+      # 10.10.236.122
+      # 10.10.179.116
+      # 10.10.151.129
+        
       # Machines
       # - leviathan
       # - neferu
       # - nimue
-      nixosConfigurations.k1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          ./hosts/knode
-          disko.nixosModules.disko
-          ./modules/partitions/single-disk-zfs-swap.nix
-          { hardware.facter.reportPath = ./hosts/knode/facter.json; }
-          { networking.hostName = "k1"; }
-          comin.nixosModules.comin
-          ({
-            services.comin = {
-              enable = true;
-              remotes = [{
-                name = "origin";
-                url = "https://github.com/ph/heyklab.git";
-              }];
-            };
-          })
-        ];
-      };
+      nixosConfigurations.k1 = kubernetes { hostName = "k1"; ip = "10.10.2.1"; };
+      nixosConfigurations.k2 = kubernetes { hostName = "k2"; ip = "10.10.2.2"; };
+      nixosConfigurations.k3 = kubernetes { hostName = "k3"; ip = "10.10.2.3"; };
 
       # Minimal Bootable ISO
       packages = forAllSystems (
@@ -70,6 +101,7 @@
 
             modules = [
               "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              { isoImage.edition = nixpkgs.lib.mkForce "vm-installer"; }
               ./modules/images/vm-boot.nix
             ];
           }).config.system.build.image;
@@ -80,6 +112,7 @@
 
             modules = [
               "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              { isoImage.edition = nixpkgs.lib.mkForce "usb-installer"; }
               ./modules/images/usb-boot.nix
             ];
           }).config.system.build.image;
@@ -99,4 +132,4 @@
       );
     };
 }
-# nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/nixos-anywhere -- --generate-hardware-config nixos-facter ./hosts/knode/facter.json  --flake .#knode-bootstrap --target-host deploy@10.10.229.88
+# nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/nixos-anywhere -- --generate-hardware-config nixos-facter ./hosts/knode/k1.json  --flake .#k1 --target-host deploy@10.10.236.122
