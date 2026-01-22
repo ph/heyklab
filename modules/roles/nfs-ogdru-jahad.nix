05aaf0ac3c952b17c879e081903a3e08fb45e7ff
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-22 11:46:29 -0500

    missing files

---
+++ modules/roles/nfs-ogdru-jahad.nix
@@ -0,0 +1,22 @@
+{ ... }:
+{
+  config = {
+    boot.supportedFilesystems = "nfs";
+    services.rpcbind.enable = true;
+
+    systemd.mounts = [{
+      type = "nfs";
+      what = "192.168.1.152:/volume1/leviathan";
+      where = "/mnt/ogdru-jahad";
+    }];
+
+    systemd.automounts = [{
+      wantedBy = [ "multi-user.target" ];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+    }];
+
+    where = "/mnt/ogdru-jahad";
+  };
+}
