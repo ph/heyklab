6b1f9c1a85e586c8112cc3a35923a91cc7d66285
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-24 17:31:46 -0500

    remove automount

--- modules/roles/nfs-ogdru-jahad.nix
+++ modules/roles/nfs-ogdru-jahad.nix
@@ -1,15 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 {
   config = {
+    environment.systemPackages = with pkgs; [ nfs-utils ];
+
+    boot.supportedFilesystems = [ "nfs" ];
+
     fileSystems."/mnt/ogdru-jahad" = {
       device = "ogdru-jahad:/volume1/leviathan";
-      fsType = "nfs";
+      fsType = "nfs4";
       options = [
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=600"
+        "defaults"
+        "nofail"
+        "vers=4.2"
       ];
-    };
-    boot.supportedFilesystems = [ "nfs" ];
+
+    #   options = [
+    #     "x-systemd.automount"
+    #     "noauto"
+    #     "x-systemd.idle-timeout=600"
+    #   ];
+    # };
+
   };
 }
