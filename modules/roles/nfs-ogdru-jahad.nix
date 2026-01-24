9e2cfee3e6e654e65fb152d4baa68972fb82f3bd
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-24 17:02:59 -0500

    inside option

--- modules/roles/nfs-ogdru-jahad.nix
+++ modules/roles/nfs-ogdru-jahad.nix
@@ -4,12 +4,12 @@
     fileSystems."/mnt/ogdru-jahad" = {
       device = "ogdru-jahad:/volume1/leviathan";
       fsType = "nfs";
+      options = [
+        "x-systemd.automount"
+        "noauto"
+        "x-systemd.idle-timeout=600"
+      ];
     };
     boot.supportedFilesystems = [ "nfs" ];
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=600"
-    ];
   };
 }
