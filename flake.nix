d36fe274256b77c57727f48e0a7b465cf91ded6f
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-25 09:14:49 -0500

    change ips

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
           { hardware.facter.reportPath = ./hosts/knode/facter.json; }
           {
             networking = {
-              #dhcpcd.enable = false;
+              dhcpcd.enable = false;
               hostName = hostName;
               interfaces.ens18.ipv4.addresses = [{
                 address = ip;
@@ -97,9 +97,9 @@
       # - nimue
 
 
-      nixosConfigurations.k1 = k { hostName = "k1"; ip = "10.10.2.11"; primary = true; };
-      nixosConfigurations.k2 = k { hostName = "k2"; ip = "10.10.2.12"; };
-      nixosConfigurations.k3 = k { hostName = "k3"; ip = "10.10.2.13"; };
+      nixosConfigurations.k1 = k { hostName = "k1"; ip = "10.10.10.11"; primary = true; };
+      nixosConfigurations.k2 = k { hostName = "k2"; ip = "10.10.10.12"; };
+      nixosConfigurations.k3 = k { hostName = "k3"; ip = "10.10.10.13"; };
 
       # Minimal Bootable ISO
       packages = forAllSystems (
