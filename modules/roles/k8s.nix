e051cb5f0557b8f9a64804e8df754777da42d86b
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-01-30 11:28:02 -0500

    toggle true

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -44,7 +44,7 @@ in {
       
       {
         # manifests.nginx.source = ../../manifests/fluxcd.yaml;
-        enable = false; 
+        enable = true; 
         role = "server";
         clusterInit = cfg.primary;
         extraFlags = toString [
