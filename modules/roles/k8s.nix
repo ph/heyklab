d5ae4539c64b3f79e4d13bb87f0935ef113c0761
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-02 10:50:38 -0500

    enable more ports

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -61,6 +61,10 @@ in {
         6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
         2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
         2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+        80 # HTTP
+        443 # HTTPS
+        5001 # Embedded registry (spegel)
+        9100 # Node exporter
       ];
 
       allowedUDPPorts = [
