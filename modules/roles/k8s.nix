82e9c0de6939cce7137fec7878fe54cf9b566bad
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-02 10:20:42 -0500

    disable nftable

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -47,7 +47,7 @@ in {
       kubernetes-helm
     ];
     
-    networking.nftables.enable = true;
+    networking.nftables.enable = false;
     networking.firewall = {
       allowedTCPPorts = [
         6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
