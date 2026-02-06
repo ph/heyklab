66b82f2b8232b849d34559a656fe3f7fbff428aa
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-06 11:37:33 -0500

    remove flannel

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -64,30 +64,30 @@ in {
       ];
     };
 
-    # virtualisation.containerd = {
-    #   enable = true;
-    #   settings =
-    #     let
-    #       fullCNIPlugins = pkgs.buildEnv {
-    #         name = "full-cni";
-    #         paths = with pkgs;[
-    #           cni-plugins
-    #           cni-plugin-flannel
-    #         ];
-    #       };
-    #     in {
-    #       plugins."io.containerd.grpc.v1.cri" = {
-    #         cni = {
-    #           bin_dir = "${fullCNIPlugins}/bin";
-    #           conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
-    #         };
+    virtualisation.containerd = {
+      enable = true;
+      settings =
+        let
+          fullCNIPlugins = pkgs.buildEnv {
+            name = "full-cni";
+            paths = with pkgs;[
+              cni-plugins
+              cni-plugin-flannel
+            ];
+          };
+        in {
+          plugins."io.containerd.grpc.v1.cri" = {
+            # cni = {
+            #   bin_dir = "${fullCNIPlugins}/bin";
+            #   conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+            # };
 
-    #         containerd = {
-    #           snapshotter = "zfs";
-    #         };
-    #       };
-    #     };
-    # };
+            containerd = {
+              snapshotter = "zfs";
+            };
+          };
+        };
+    };
 
     services.k3s = lib.mkMerge [
       {
