97404b2c5b55b556df49f1f09c3cbc729d356fe9
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-06 11:29:29 -0500

    enable back

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -91,7 +91,7 @@ in {
 
     services.k3s = lib.mkMerge [
       {
-        enable = false;
+        enable = true;
         role = "server";
         clusterInit = cfg.primary;
         extraFlags = [
