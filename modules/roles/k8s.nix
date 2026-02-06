2c7a3b2c170354f510a1933c04f8b01689eb050f
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-06 10:10:26 -0500

    add hash for cilium helm

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -114,7 +114,7 @@ in {
           name = "cilium";
           repo = "https://helm.cilium.io";
           version = "1.19.0";
-          hash = "sha256-U2XjNEWE82/Q3KbBvZLckXbtjsXugUbK6KdqT5kCccM=";
+          hash = "sha256-W3dPDguTrXEnFmzawbrFtktbmsZgy6SrA2O5rH9Vo34=";
           values = {
             cni.exclusive = false;
             operator.replicas = 1;
