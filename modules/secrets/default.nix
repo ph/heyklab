02a8d1e2158f0a85cef7780a242a5f739d522598
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-09 13:47:52 -0500

    secret work

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -1,21 +1,21 @@
 { config, ... }:
 {
-  # sops = {
-  #   secrets.github-token = { };
-  #   templates.password = {
-  #     content = builtins.toJSON {
-  #       apiVersion = "v1";
-  #       kind = "Secret";
-  #       metadata = {
-  #        name = "github-token"; 
-  #        namespace = "flux-system";
-  #       };
-  #       stringData = {
-  #         username = "ph";
-  #         password = config.sops.placeholder.github-token;
-  #       };
-  #     };
-  #     path = "/var/lib/rancher/k3s/server/manifests/github-token.json";
-  #   };
-  # };
+  sops = {
+    secrets.github-token = { };
+    templates.password = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+         name = "github-token"; 
+         namespace = "flux-system";
+        };
+        stringData = {
+          username = "ph";
+          password = config.sops.placeholder.github-token;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/github-token.json";
+    };
+  };
 }
