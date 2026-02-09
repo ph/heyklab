4b646106587e1e33d92bcfd2b77737015ed9a8e1
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-09 13:14:09 -0500

    move it back to false

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -93,7 +93,7 @@ in {
 
     services.k3s = lib.mkMerge [
       {
-        enable = true;
+        enable = false;
         role = "server";
         clusterInit = cfg.primary;
         extraFlags = [
