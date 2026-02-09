bea420e88578807eba6021f33fe412d50a8105ec
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-09 15:59:14 -0500

    check bgp

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -66,31 +66,6 @@ in {
       ];
     };
 
-    # virtualisation.containerd = {
-    #   enable = true;
-    #   settings =
-    #     let
-    #       fullCNIPlugins = pkgs.buildEnv {
-    #         name = "full-cni";
-    #         paths = with pkgs;[
-    #           cni-plugins
-    #           cni-plugin-flannel
-    #         ];
-    #       };
-    #     in {
-    #       plugins."io.containerd.grpc.v1.cri" = {
-    #         # cni = {
-    #         #   bin_dir = "${fullCNIPlugins}/bin";
-    #         #   conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
-    #         # };
-
-    #         containerd = {
-    #           snapshotter = "zfs";
-    #         };
-    #       };
-    #     };
-    # };
-
     services.k3s = lib.mkMerge [
       {
         enable = true;
@@ -144,7 +119,7 @@ in {
           };
         };
 
-        # manifests.ciliumbgp.source = ../../manifests/cilium-bgp.yaml;
+        manifests.ciliumbgp.source = ../../manifests/cilium-bgp.yaml;
       })
       {
         manifests.fluxoperator.source = ../../manifests/flux-operator.yaml;
