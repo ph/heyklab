3bff1a8d317cede5ef176bc2a7b21eb7edb19019
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-11 09:48:56 -0500

    ensure from scratch still work

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -68,7 +68,7 @@ in {
 
     services.k3s = lib.mkMerge [
       {
-        enable = true;
+        enable = false;
         role = "server";
         clusterInit = cfg.primary;
         extraFlags = [
