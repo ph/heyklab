d2b5e7b9afda8dece7d1a18f1383c39bf5dc1c5b
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-13 09:56:09 -0500

    enable gatewayapi for cert-manager

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -156,9 +156,13 @@ in {
               crds:
                 enabled: true
             '';
+            config = {
+              apiVersion = "controller.config.cert-manager.io/v1alpha1";
+              kind = "ControllerConfiguration";
+              enableGatewayAPI = true;
+            };
           };
         };
-
       })
       {
         manifests.fluxoperator.source = ../../manifests/flux-operator.yaml;
