56ce2a0c72d4c4f9e27450dce3598169d5e15177
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-16 14:01:55 -0500

    add another port

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -54,6 +54,7 @@ in {
       checkReversePath = false;
       trustedInterfaces = [ "cni+" ];
       allowedTCPPorts = [
+        4240 # cilium
         6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
         2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
         2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
