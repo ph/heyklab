676b49c07e78340e7b99d736b0250ce8da6a738b
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-17 10:27:36 -0500

    disable mount nfs..

--- modules/roles/nfs-ogdru-jahad.nix
+++ modules/roles/nfs-ogdru-jahad.nix
@@ -1,31 +1,31 @@
 { pkgs, ... }:
 {
-  config = {
-    environment.systemPackages = with pkgs; [ nfs-utils ];
-    boot.supportedFilesystems = [ "nfs" ];
+  # config = {
+  #   environment.systemPackages = with pkgs; [ nfs-utils ];
+  #   boot.supportedFilesystems = [ "nfs" ];
 
-    fileSystems."/mnt/ogdru-jahad/servers" = {
-      device = "ogdru-jahad:/volume1/servers";
-      fsType = "nfs4";
-      options = [
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=60"
-        "x-systemd.device-timeout=5s"
-        "x-systemd.mount-timeout=5s"
-      ];
-    };
+  #   fileSystems."/mnt/ogdru-jahad/servers" = {
+  #     device = "ogdru-jahad:/volume1/servers";
+  #     fsType = "nfs4";
+  #     options = [
+  #       "x-systemd.automount"
+  #       "noauto"
+  #       "x-systemd.idle-timeout=60"
+  #       "x-systemd.device-timeout=5s"
+  #       "x-systemd.mount-timeout=5s"
+  #     ];
+  #   };
 
-    fileSystems."/mnt/ogdru-jahad/music" = {
-      device = "ogdru-jahad:/volume1/Music";
-      fsType = "nfs4";
-      options = [
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=60"
-        "x-systemd.device-timeout=5s"
-        "x-systemd.mount-timeout=5s"
-      ];
-    };
-  };
+  #   fileSystems."/mnt/ogdru-jahad/music" = {
+  #     device = "ogdru-jahad:/volume1/Music";
+  #     fsType = "nfs4";
+  #     options = [
+  #       "x-systemd.automount"
+  #       "noauto"
+  #       "x-systemd.idle-timeout=60"
+  #       "x-systemd.device-timeout=5s"
+  #       "x-systemd.mount-timeout=5s"
+  #     ];
+  #   };
+  # };
 }
