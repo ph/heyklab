17d3846ef682497be26dadce9d401e2e6a38e4d0
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-17 09:21:19 -0500

    havent run everything from the beginning for some time

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -85,7 +85,7 @@ in {
 
     services.k3s = lib.mkMerge [
       {
-        enable = true;
+        enable = false;
         role = "server";
         clusterInit = cfg.primary;
         extraFlags = [
