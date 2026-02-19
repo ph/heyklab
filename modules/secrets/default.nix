8ee7f69511839797f49e098b569a5bb5ac6f9457
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-19 16:45:03 -0500

    disable token

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -20,38 +20,38 @@
     };
 
     # Secrets for Google DNS ACME.
-    secrets.google-dns-key = { };
-    templates.google-dns-key = {
-      content = builtins.toJSON {
-        apiVersion = "v1";
-        kind = "Secret";
-        metadata = {
-         name = "google-dns-key"; 
-         namespace = "cert-manager";
-        };
+    # secrets.google-dns-key = { };
+    # templates.google-dns-key = {
+    #   content = builtins.toJSON {
+    #     apiVersion = "v1";
+    #     kind = "Secret";
+    #     metadata = {
+    #      name = "google-dns-key"; 
+    #      namespace = "cert-manager";
+    #     };
 
-        data = {
-          "key.json" = config.sops.placeholder.google-dns-key;
-        };
-      };
-      path = "/var/lib/rancher/k3s/server/manifests/google-dns-key.json";
-    };
+    #     data = {
+    #       "key.json" = config.sops.placeholder.google-dns-key;
+    #     };
+    #   };
+    #   path = "/var/lib/rancher/k3s/server/manifests/google-dns-key.json";
+    # };
 
-    # Secrets for Garage
-    secrets.garage-admin-token = { };
-    templates.garage-admin-token = {
-      content = builtins.toJSON {
-        apiVersion = "v1";
-        kind = "Secret";
-        metadata = {
-         name = "garage-admin-token"; 
-         namespace = "default";
-        };
-        data = {
-          token = config.sops.placeholder.garage-admin-token;
-        };
-      };
-      path = "/var/lib/rancher/k3s/server/manifests/garage-admin-token.json";
-    };
+    # # Secrets for Garage
+    # secrets.garage-admin-token = { };
+    # templates.garage-admin-token = {
+    #   content = builtins.toJSON {
+    #     apiVersion = "v1";
+    #     kind = "Secret";
+    #     metadata = {
+    #      name = "garage-admin-token"; 
+    #      namespace = "default";
+    #     };
+    #     data = {
+    #       token = config.sops.placeholder.garage-admin-token;
+    #     };
+    #   };
+    #   path = "/var/lib/rancher/k3s/server/manifests/garage-admin-token.json";
+    # };
   };
 }
