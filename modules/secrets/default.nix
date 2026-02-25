64cd26d5842dc79e154c5779e1bac3d1854e922d
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-25 13:53:44 -0500

    move into flux

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -20,73 +20,76 @@
       path = "/var/lib/rancher/k3s/server/manifests/github-token.json";
     };
 
-    # # Secrets for Google DNS ACME.
-    # secrets.google-dns-key = { };
-    # templates.google-dns-key = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "google-dns-key"; 
-    #      namespace = "cert-manager";
-    #     };
 
-    #     data = {
-    #       "key.json" = config.sops.placeholder.google-dns-key;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/google-dns-key.json";
-    # };
 
-    # # Secrets for Garage
-    # secrets.garage-admin-token = { };
-    # templates.garage-admin-token = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "garage-admin-token"; 
-    #      namespace = "default";
-    #     };
-    #     stringData = {
-    #       token = config.sops.placeholder.garage-admin-token;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/garage-admin-token.json";
-    # };
+    # TODO: move into Flux
+    # Secrets for Google DNS ACME.
+    secrets.google-dns-key = { };
+    templates.google-dns-key = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+         name = "google-dns-key"; 
+         namespace = "cert-manager";
+        };
 
-    # # Secrets for Garage
-    # secrets.database-grafana-password = { };
-    # templates.database-grafana-password = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "database-grafana-password"; 
-    #      namespace = "pg";
-    #     };
-    #     stringData = {
-    #       username = "grafana";
-    #       password = config.sops.placeholder.database-grafana-password;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password.json";
-    # };
+        data = {
+          "key.json" = config.sops.placeholder.google-dns-key;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/google-dns-key.json";
+    };
 
-    # templates.database-grafana-password-monitoring = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #       name = "database-grafana-password"; 
-    #       namespace = "monitoring";
-    #     };
-    #     stringData = {
-    #       username = "grafana";
-    #       password = config.sops.placeholder.database-grafana-password;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password-monitoring.json";
-    # };
+    # Secrets for Garage
+    secrets.garage-admin-token = { };
+    templates.garage-admin-token = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+         name = "garage-admin-token"; 
+         namespace = "default";
+        };
+        stringData = {
+          token = config.sops.placeholder.garage-admin-token;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/garage-admin-token.json";
+    };
+
+    # Secrets for Garage
+    secrets.database-grafana-password = { };
+    templates.database-grafana-password = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+         name = "database-grafana-password"; 
+         namespace = "pg";
+        };
+        stringData = {
+          username = "grafana";
+          password = config.sops.placeholder.database-grafana-password;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password.json";
+    };
+
+    templates.database-grafana-password-monitoring = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+          name = "database-grafana-password"; 
+          namespace = "monitoring";
+        };
+        stringData = {
+          username = "grafana";
+          password = config.sops.placeholder.database-grafana-password;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password-monitoring.json";
+    };
  };
 }
