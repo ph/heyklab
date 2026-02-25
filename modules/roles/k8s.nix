7132c3c4d2e17e2f2e26d1405ec648379fa26bad
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-25 13:07:01 -0500

    add flux instance configuration

--- modules/roles/k8s.nix
+++ modules/roles/k8s.nix
@@ -175,7 +175,7 @@ in {
         };
 
         # Load Flux configuration from our infrastructure configuration.
-        # manifests.flux.source = ../../clusters/infrastructure/configs/flux-instance.yaml;
+        manifests.flux.source = ../../clusters/infrastructure/configs/flux-instance.yaml;
       }
 
       (lib.mkIf (cfg.mainServer != "" && !cfg.primary) {
