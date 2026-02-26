d9b59fff0b95e3c942cbe7d95ccf715b6c5b452c
Pier-Hugues Pellerin <phpellerin@gmail.com>
2026-02-26 17:55:23 -0500

    Use sops for k8s secrets

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -19,80 +19,21 @@
       path = "/var/lib/rancher/k3s/server/manifests/github-token.json";
     };
 
-
-
-    # # TODO: move into Flux
-    # # Secrets for Google DNS ACME.
-    # secrets.google-dns-key = { };
-    # templates.google-dns-key = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "google-dns-key"; 
-    #      namespace = "cert-manager";
-    #      createNamespace = true;
-    #     };
-
-    #     data = {
-    #       "key.json" = config.sops.placeholder.google-dns-key;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/google-dns-key.json";
-    # };
-
-    # # Secrets for Garage
-    # secrets.garage-admin-token = { };
-    # templates.garage-admin-token = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "garage-admin-token"; 
-    #      namespace = "default";
-    #      createNamespace = true;
-    #     };
-    #     stringData = {
-    #       token = config.sops.placeholder.garage-admin-token;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/garage-admin-token.json";
-    # };
-
-    # # Secrets for Garage
-    # secrets.database-grafana-password = { };
-    # templates.database-grafana-password = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #      name = "database-grafana-password"; 
-    #      namespace = "pg";
-    #      createNamespace = true;
-    #     };
-    #     stringData = {
-    #       username = "grafana";
-    #       password = config.sops.placeholder.database-grafana-password;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password.json";
-    # };
-
-    # templates.database-grafana-password-monitoring = {
-    #   content = builtins.toJSON {
-    #     apiVersion = "v1";
-    #     kind = "Secret";
-    #     metadata = {
-    #       name = "database-grafana-password"; 
-    #       namespace = "monitoring";
-    #       createNamespace = true;
-    #     };
-    #     stringData = {
-    #       username = "grafana";
-    #       password = config.sops.placeholder.database-grafana-password;
-    #     };
-    #   };
-    #   path = "/var/lib/rancher/k3s/server/manifests/database-grafana-password-monitoring.json";
-    # };
+    # Token for `comin` service to pull new nix config.
+    secrets.sops-agekey = { };
+    templates.sops-agekey = {
+      content = builtins.toJSON {
+        apiVersion = "v1";
+        kind = "Secret";
+        metadata = {
+         name = "sops-agekey"; 
+         namespace = "flux-system";
+        };
+        stringData = {
+          "age.agekey" = config.sops.placeholder.sops-agekey;
+        };
+      };
+      path = "/var/lib/rancher/k3s/server/manifests/sops-agekey.json";
+    };
  };
 }
